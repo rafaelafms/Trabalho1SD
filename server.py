e81d60928e7ce33029b352e11a003ae5ab369e7f
Shikiyo <shikiyo@archjk>
2023-06-26 12:07:09 -0300

    Versao final 0

--- server.py
+++ server.py
@@ -1,12 +1,11 @@
 from __future__ import annotations
 
-from typing import Generic, Optional, Tuple, TypeAlias, TypeVar, Union
+from typing import Optional, TypeAlias
 from dataclasses import dataclass, field
 
 import rpyc # type: ignore
 
 import threading
-import time
 import sys
 
 from interface import UserId, Topic, Content, FnNotify, BrokerService
@@ -20,18 +19,12 @@ def log(s: str) -> None:
         print(s, file=sys.stderr)
 
 def log_contents(
-        contents: Union[list[Content], LimitedBuffer[Content]],
+        contents: list[Content],
         ) -> None:
     if not GLOBAL_LOG:
         return
-    if isinstance(contents, list):
-        for i, content in enumerate(contents):
-            log(f"#{i}: {content}")
-    elif isinstance(contents, LimitedBuffer):
-        for slot in contents.list_start(0):
-            log(f"#{slot.id}: {slot.item}")
-    else:
-        assert False, "unreachable"
+    for i, content in enumerate(contents):
+        log(f"#{i}: {content}")
 
 IndexNext: TypeAlias = int
 
@@ -42,97 +35,14 @@ class SubsState:
 
 Subscribers: TypeAlias = dict[UserId, SubsState]
 
-Ip: TypeAlias = int
-
-T = TypeVar('T')
-U = TypeVar('U')
-
-@dataclass(kw_only=True, slots=True)
-class LimitedBuffer(Generic[T]):
-    MAX_LEN = 3
-
-    @dataclass(frozen=True, kw_only=True, slots=True)
-    class Item(Generic[U]):
-        id: int
-        item: U
-
-    buf: list[Item[T]] = field(default_factory=list)
-    next_id: int = 0
-    last_idx: int = 0
-
-    def convert_index(self, i: int) -> int:
-        assert 0 <= i
-        last_item: LimitedBuffer.Item[T] = self.buf[self.last_idx]
-        assert last_item.id <= i and i < self.next_id
-        last_id: int = self.buf[self.last_idx].id
-        idx: int = (i - last_id + self.last_idx) % LimitedBuffer.MAX_LEN
-        return idx
-
-    def __getitem__(self, i: Union[int, slice]) -> Union[None, T, list[T]]:
-        if isinstance(i, int):
-            assert 0 <= i
-            last_item: LimitedBuffer.Item[T] = self.buf[self.last_idx]
-            if last_item.id <= i and i < self.next_id:
-                idx: int = self.convert_index(i)
-                return self.buf[idx].item
-            else:
-                return None
-        elif isinstance(i, slice):
-            assert i.stop is None
-            assert i.step is None
-            start: int = i.start or 0
-            return [
-                x.item
-                for x in self.list_start(start)
-            ]
-        else:
-            assert False, f"Unhandled type {type(i)}"
-
-    def __len__(self) -> int:
-        return self.next_id
-
-    def append(self, object: T) -> None:
-        item: LimitedBuffer.Item[T] = LimitedBuffer.Item(
-            id = self.next_id,
-            item = object,
-        )
-        if LimitedBuffer.MAX_LEN <= self.next_id:
-            self.buf[self.last_idx] = item
-            self.last_idx = (self.last_idx + 1) % LimitedBuffer.MAX_LEN
-        else:
-            self.buf.append(item)
-        self.next_id += 1
-
-    def list_start(self, start: int) -> list[LimitedBuffer.Item[T]]:
-        assert 0 <= start
-        last_item: LimitedBuffer.Item[T] = self.buf[self.last_idx]
-        start_small: Optional[int] = None
-        if start < last_item.id:
-            start_small = last_item.id
-        elif last_item.id <= start and start < self.next_id:
-            start_small = self.convert_index(start)
-        else:
-            start_small = LimitedBuffer.MAX_LEN
-        assert start_small is not None
-        list_small: list[LimitedBuffer.Item[T]] = self.buf[start_small:]
-        list_big: list[LimitedBuffer.Item[T]] = self.buf[0:self.last_idx]
-        return list_small + list_big
-
-# BufferType: TypeAlias = LimitedBuffer
-# Buffer = LimitedBuffer
 BufferType: TypeAlias = list
 Buffer = list
-
-from typing import Any
-def magia(*a: Any) -> Any:
-    assert False, "Magia should not be called"
 
 class ME(BrokerService):
     all_topics: list[Topic] = list()
     all_contents: BufferType[Content] = Buffer()
     all_subs: Subscribers = dict()
     logged_in: dict[rpyc.Connection, UserId] = dict()
-    notify_queue: list[Topic] = list()
 
     def create_topic(self, id: UserId, topic: Topic) -> Topic:
         if topic not in self.all_topics:
@@ -168,15 +78,7 @@ class ME(BrokerService):
                     callback = callback
                 )
             else:
-                user_callback = ME.all_subs[id].callback
-                if user_callback is None:
-                    log(f"> '{id}' already logged out")
-                else:
-                    if ME.test_callback(user_callback):
-                        log(f"> '{id}' is connected")
-                        return False
-                    else:
-                        log(f"> '{id}' just disconnected")
+                assert ME.all_subs[id].callback is None
                 ME.all_subs[id].callback = callback
                 self.notify_conn()
             return True
@@ -202,9 +104,6 @@ class ME(BrokerService):
             log(f"> Publishing {content}")
             log_contents(ME.all_contents)
             self.notify_conn()
-            # ME.notify_all(content.topic)
-            # ME.notify_one(ME.all_subs[author], content.topic)
-            # self.notify_queue.append(content.topic)
             return True
 
     def exposed_subscribe_to(self, id: UserId, topic: Topic) -> bool:
@@ -236,12 +135,6 @@ class ME(BrokerService):
         self.notify_conn()
         return True
 
-    @staticmethod
-    def notify_all(topic: Topic) -> None:
-        log(f"notify all: '{topic}'")
-        for subs_state in ME.all_subs.values():
-            ME.notify_one(subs_state, topic)
-
     def notify_conn(self) -> None:
         id = ME.logged_in[self._conn]
         subs_state = ME.all_subs[id]
@@ -265,37 +158,8 @@ class ME(BrokerService):
                         log(f"> append: {content}")
                         list_to_send.append(content)
                 if len(list_to_send) > 0:
-                    if ME.test_callback_with(subs_state.callback, list_to_send):
-                        subs_state.subscribed[topic] = len(ME.all_contents)
-                    else:
-                        log(f"> someone was disconnected")
-                        subs_state.callback = None
-
-    @staticmethod
-    def test_callback(callback: FnNotify) -> bool:
-        return ME.test_callback_with(callback, [])
-
-    @staticmethod
-    def test_callback_with(callback: FnNotify, list_send: list[Content]) -> bool:
-        log('test_callback')
-        ok: bool = False
-        try:
-            log('> test_callback: try')
-            callback(list_send)
-            log(f"> test_callback: callback({list_send})")
-            ok = True
-        except Exception as e:
-            log(f"> test_callback: except {e}")
-        return ok
-
-def notify() -> None:
-    while True:
-        while len(ME.notify_queue) > 0:
-            topic: Topic = ME.notify_queue.pop(0)
-            log(f"thread notify '{topic}'")
-            ME.notify_all(topic)
-            log(f"thread notify: is sleeping")
-        time.sleep(1)
+                    subs_state.callback(list_to_send)
+                    subs_state.subscribed[topic] = len(ME.all_contents)
 
 def main() -> int:
     from rpyc.utils.server import ThreadedServer # type: ignore
@@ -314,24 +178,10 @@ def main() -> int:
         threading.Thread(
             target=srv.start,
         )
-    notify_thread: threading.Thread = \
-        threading.Thread(
-            target=notify,
-            args=(),
-        )
     server_thread.start()
-    # notify_thread.start()
     server_thread.join()
-    # notify_thread.join()
     return 0
 
 if __name__ == "__main__":
     retcode: int = main()
     sys.exit(retcode)
-
-# TODO LIST:
-# notify "assincrono"
-# "select" para o RPyC
-# Magia do ip
-
-# hashi: mirror
